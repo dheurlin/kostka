90c99ea4161d61cf3e55bff688ed94c467ebda37
dheurlin <daniel.heurlin@gmail.com>
2022-12-23 22:06:02 +0100

    support newer base + brick version

--- src/Tui.hs
+++ src/Tui.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings, NumericUnderscores #-}
+{-# LANGUAGE TemplateHaskell, LambdaCase #-}
 
 module Tui ( tui ) where
 
@@ -10,6 +11,8 @@ import Control.Monad      ( forever, void )
 import Data.Time.Clock
 import Data.Time.Format
 
+import Control.Lens
+
 import Brick.AttrMap
 import Brick.Main
 import Brick.Types
@@ -20,16 +23,6 @@ import Brick.Widgets.Center
 import Brick.Widgets.Border
 import Graphics.Vty
 
-
--- | Draws the Kostka TUI
-tui :: IO ()
-tui = do
-  eventChan    <- Brick.BChan.newBChan 10
-  initialState <- buildInitialState eventChan
-  let buildVty = Graphics.Vty.mkVty Graphics.Vty.defaultConfig
-  initialVty <- buildVty
-
-  void $ customMain initialVty buildVty (Just eventChan) tuiApp initialState
 
 -- Generates an event for the countdown until the timer starts
 countdownTick :: BChan CustomEvent -> IO ()
@@ -59,16 +52,18 @@ timeFormat ts =
 
 data CustomEvent = CountDown Int | TimerTick String
 
-data TuiState = TuiState
-  { tuiStateShuffle     :: String
-  , tuiStateMain        :: MainState
-  , tuiStateEventChan   :: BChan CustomEvent
-  , tuiStateTickThread  :: Maybe ThreadId
-  , tuiStateTime        :: String
-  }
-
 data MainState = Passive | CountingDown Int | Running
   deriving (Eq, Show)
+
+data TuiState = TuiState
+  { _tuiStateShuffle     :: String
+  , _tuiStateMain        :: MainState
+  , _tuiStateEventChan   :: BChan CustomEvent
+  , _tuiStateTickThread  :: Maybe ThreadId
+  , _tuiStateTime        :: String
+  }
+
+makeLenses ''TuiState
 
 data ResourceName =
   ResourceName
@@ -80,14 +75,14 @@ tuiApp =
     { appDraw         = drawTui
     , appChooseCursor = showFirstCursor
     , appHandleEvent  = handleTuiEvent
-    , appStartEvent   = pure
-    , appAttrMap      = const $ attrMap mempty ourAttrMap
+    , appStartEvent   = pure ()
+    , appAttrMap      = const $ attrMap Graphics.Vty.defAttr ourAttrMap
     }
 
 ourAttrMap :: [(AttrName, Attr)]
 ourAttrMap =
-  [ ("holding", fg red )
-  , ("held"   , fg green)
+  [ (attrName "holding", fg red )
+  , (attrName "held"   , fg green)
   ]
 
 newShuffle :: IO String
@@ -97,17 +92,17 @@ buildInitialState :: BChan CustomEvent -> IO TuiState
 buildInitialState chan = do
   shuffle <- newShuffle
   pure $ TuiState
-    { tuiStateShuffle     = shuffle
-    , tuiStateMain        = Passive
-    , tuiStateEventChan = chan
-    , tuiStateTickThread  = Nothing
-    , tuiStateTime        = "0.00"
+    { _tuiStateShuffle     = shuffle
+    , _tuiStateMain        = Passive
+    , _tuiStateEventChan = chan
+    , _tuiStateTickThread  = Nothing
+    , _tuiStateTime        = "0.00"
     }
 
 drawTui :: TuiState -> [Widget ResourceName]
 drawTui ts =
   let border = borderWithLabel (str " Kostka Rubik's Cube Timer ")
-      shuffle = hCenter $ padTop (Pad 1) $ str (tuiStateShuffle ts)
+      shuffle = hCenter $ padTop (Pad 1) $ str (_tuiStateShuffle ts)
       hold    = holdWidget ts
       time    = timeWidget ts
       contents =
@@ -118,54 +113,65 @@ drawTui ts =
 
 -- The widget prompting the user to press space
 holdWidget :: TuiState -> Widget n
-holdWidget TuiState{tuiStateMain = m} =
+holdWidget TuiState{_tuiStateMain = m} =
   let skeleton = hCenter . padTop (Pad 1) . str
       holdText = "press space to start countdown"
    in case m of
         Passive        -> skeleton holdText
-        CountingDown n -> withAttr "holding" . skeleton $ show n
+        CountingDown n -> withAttr (attrName "holding") . skeleton $ show n
         Running        -> str ""
 
 -- The widget displaying the main timer
 timeWidget :: TuiState -> Widget n
-timeWidget TuiState{tuiStateTime = s} = hCenter $ padTop (Pad 1) $ str s
+timeWidget TuiState{_tuiStateTime = s} = hCenter $ padTop (Pad 1) $ str s
 
 -- Handle keyboard and custom events
-handleTuiEvent :: TuiState -> BrickEvent n CustomEvent -> EventM n (Next TuiState)
+handleTuiEvent ::  BrickEvent n CustomEvent -> EventM n TuiState ()
 
 -- Handle the countdown event, i.e. the countdown until the timer starts
-handleTuiEvent s (AppEvent (CountDown 0)) = do
+handleTuiEvent (AppEvent (CountDown 0)) = do
   -- start a new thread for the timer and save its PID in the state
-  threadId <- liftIO $ forkIO $ timerTick (tuiStateEventChan s)
-  continue $ s { tuiStateMain       = Running
-               , tuiStateTickThread = Just threadId
-               }
-handleTuiEvent s (AppEvent (CountDown n)) = continue $ s { tuiStateMain = CountingDown n }
+  threadId <- liftIO . forkIO . timerTick =<< use tuiStateEventChan
+  tuiStateMain .= Running
+  tuiStateTickThread .= Just threadId
+handleTuiEvent (AppEvent (CountDown n)) =  tuiStateMain .= CountingDown n
 
 -- Handle the timer tick to update the UI state
-handleTuiEvent s (AppEvent (TimerTick t)) = continue $ s { tuiStateTime = t }
+handleTuiEvent (AppEvent (TimerTick t)) = tuiStateTime .= t
 
 -- Handle keyboard events
-handleTuiEvent s (VtyEvent vtye) =
+handleTuiEvent (VtyEvent vtye) =
   case vtye of
-    EvKey (KChar 'q') [] -> halt s
+    EvKey (KChar 'q') [] -> Brick.Main.halt
     EvKey (KChar 's') [] -> do
       shuffle' <- liftIO newShuffle
-      continue $ s { tuiStateShuffle = shuffle' }
-    EvKey (KChar ' ') [] -> case tuiStateMain s of
+      tuiStateShuffle .= shuffle'
+    EvKey (KChar ' ') [] -> use tuiStateMain >>= \case
         -- Start the countdown
         Passive -> do
-          liftIO $ forkIO $ countdownTick (tuiStateEventChan s)
-          continue $ s { tuiStateMain = CountingDown 3 }
+          s <- get
+          liftIO $ forkIO $ countdownTick (_tuiStateEventChan s)
+          put $ s { _tuiStateMain = CountingDown 3 }
         -- Stop the timer
         Running -> do
           -- kill the timer thread
-          maybe (pure ()) (liftIO . killThread) (tuiStateTickThread s)
+          maybe (pure ()) (liftIO . killThread) =<< use tuiStateTickThread
           shuffle' <- liftIO newShuffle
-          continue $ s { tuiStateMain       = Passive
-                       , tuiStateTickThread = Nothing
-                       , tuiStateShuffle    = shuffle'
-                       }
-        _ -> continue s
-    _                    -> continue s
+          tuiStateMain .= Passive
+          tuiStateTickThread .= Nothing
+          tuiStateShuffle .= shuffle'
+        _ -> pure ()
+    _                    -> pure ()
+
+handleTuiEvent _ = pure ()
+
+-- | Draws the Kostka TUI
+tui :: IO ()
+tui = do
+  eventChan    <- Brick.BChan.newBChan 10
+  initialState <- buildInitialState eventChan
+  let buildVty = Graphics.Vty.mkVty Graphics.Vty.defaultConfig
+  initialVty <- buildVty
+
+  void $ customMain initialVty buildVty (Just eventChan) tuiApp initialState
 
